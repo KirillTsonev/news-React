e9a95a58422f2f5a4277bb26479a67f4c0134e44
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-12-11 20:16:55 +0200

    Change

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import './App.sass';
 import HeaderTop from "../headerTop/HeaderTop"
+import HeaderMiddle from '../headerMiddle/headerMiddle';
 
 function App() {
   return (
     <div className='container'>
       <HeaderTop></HeaderTop>
+      <HeaderMiddle></HeaderMiddle>
     </div>
   );
 }
--- src/components/headerMiddle/headerMiddle.jsx
+++ src/components/headerMiddle/headerMiddle.jsx
@@ -1,13 +1,20 @@
-import fire from "../../icons/Fire.svg"
-import dot from "../../icons/dot.svg"
-
 import "./headerMiddle.sass"
 
 const HeaderMiddle = () => {
     return (
         <div className="headerContainer">
-            <div className=""></div>
-            <div className=""></div>
+            <div className="headerContainer__left">
+                <div className="headerContainer__left-item">війна</div>
+                <div className="headerContainer__left-item">новини</div>
+                <div className="headerContainer__left-item">політика</div>
+                <div className="headerContainer__left-item">економіка</div>
+                <div className="headerContainer__left-item">суспільство</div>
+                <div className="headerContainer__left-item">погляди</div>
+                <div className="headerContainer__left-item">світ</div>
+            </div>
+            <div className="headerContainer__right">
+                <div  className="headerContainer__right-item">LIVE</div>
+            </div>
         </div>
     )
 }
