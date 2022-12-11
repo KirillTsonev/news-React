0eba5320364aabd3db14a466ebba57ed09d6daa3
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-12-11 19:55:28 +0200

    Change

---
+++ src/components/headerMiddle/headerMiddle.jsx
@@ -0,0 +1,15 @@
+import fire from "../../icons/Fire.svg"
+import dot from "../../icons/dot.svg"
+
+import "./headerMiddle.sass"
+
+const HeaderMiddle = () => {
+    return (
+        <div className="headerContainer">
+            <div className=""></div>
+            <div className=""></div>
+        </div>
+    )
+}
+
+export default HeaderMiddle
