f183dc95d83af7ca934d4436140173c282db7aca
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-12-12 02:01:57 +0200

    Change

--- src/components/videoSection/VideoSection.jsx
+++ src/components/videoSection/VideoSection.jsx
@@ -1,4 +1,8 @@
 import mainCard from "../../img/videobg.jpg"
+import card1 from "../../img/card1.png"
+import card2 from "../../img/card2.png"
+import card3 from "../../img/card3.png"
+import card4 from "../../img/card4.png"
 
 import "./videoSection.sass"
 
@@ -15,6 +19,26 @@ const VideoSection = () => {
                         <div className="videoSectionContainer__grid__main-img"><img src={mainCard} alt="Card"/></div>
                         <div className="videoSectionContainer__grid__main-date">05 серпня 11:00</div>
                         <div className="videoSectionContainer__grid__main-heading">"Це не Джошуа": Усик розповів, кого вважає своїм головним суперником</div>
+                    </div>
+                    <div className="videoSectionContainer__grid__item">
+                        <div className="videoSectionContainer__grid__item-img"><img src={card1} alt="Card" /></div>
+                        <div className="videoSectionContainer__grid__item-date">05 серпня 11:00</div>
+                        <div className="videoSectionContainer__grid__item-heading">Ватажок "ДНР" підтвердив підготовку самосуду над полоненим...</div>
+                    </div>
+                    <div className="videoSectionContainer__grid__item">
+                        <div className="videoSectionContainer__grid__item-img"><img src={card3} alt="Card" /></div>
+                        <div className="videoSectionContainer__grid__item-date">05 серпня 11:00</div>
+                        <div className="videoSectionContainer__grid__item-heading">"Спекотні літні канікули": Міноборони України попередило...</div>
+                    </div>
+                    <div className="videoSectionContainer__grid__item">
+                        <div className="videoSectionContainer__grid__item-img"><img src={card2} alt="Card" /></div>
+                        <div className="videoSectionContainer__grid__item-date">05 серпня 11:00</div>
+                        <div className="videoSectionContainer__grid__item-heading">Під москвою палала військова частина, де проходять службу...</div>
+                    </div>
+                    <div className="videoSectionContainer__grid__item">
+                        <div className="videoSectionContainer__grid__item-img"><img src={card4} alt="Card" /></div>
+                        <div className="videoSectionContainer__grid__item-date">05 серпня 11:00</div>
+                        <div className="videoSectionContainer__grid__item-heading">СБУ затримала в Києві "військового експерта", який працював на росію...</div>
                     </div>
                 </div>
             </div>
