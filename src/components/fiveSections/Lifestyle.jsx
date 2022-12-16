a07509e44171f3b4d2615e58ff2427a688b153b9
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-12-16 23:06:30 +0200

    Change

--- src/components/fiveSections/Lifestyle.jsx
+++ src/components/fiveSections/Lifestyle.jsx
@@ -1,19 +1,23 @@
 import card from "../../img/card.png"
+import lifestyleCard from "../../img/lifestyleCard.png"
 
 const Lifestyle = () => {
     return (
         <div className="fiveSectionsContainer__blocks">
             <div className="fiveSectionsContainer__block">
                 <div className="fiveSectionsContainer__block-header">
-                    <div className="fiveSectionsContainer__block-heading">Політика</div>
+                    <div className="fiveSectionsContainer__block-heading">Лайфстайл</div>
                     <div className="fiveSectionsContainer__block-all">Всі новини розділу</div>
                 </div>
                 <div className="fiveSectionsContainer__block__grid">
                     <div className="fiveSectionsContainer__block__grid-columnOne">
                         <div className="fiveSectionsContainer__block__grid__mainCard">
-                            <img src={card} alt="card" className="fiveSectionsContainer__block__grid__mainCard-img"/>
-                            <div className="fiveSectionsContainer__block__grid__mainCard-date">05 серпня 11:00</div>
-                            <div className="fiveSectionsContainer__block__grid__mainCard-desc">У "Слузі" пояснили, чому в росії знову заговорили про переговори з Україною</div>
+                            <img src={lifestyleCard} alt="card" className="fiveSectionsContainer__block__grid__mainCard-img"/>
+                            <div className="fiveSectionsContainer__block__grid__mainCard-header">
+                                <div className="fiveSectionsContainer__block__grid__mainCard-date">05 серпня 11:00</div>
+                                <div className="fiveSectionsContainer__block__grid__mainCard-interview">Інтерв’ю</div>
+                            </div>
+                            <div className="fiveSectionsContainer__block__grid__mainCard-desc">Селезньов розповів, навіщо росіяни спустошують склади боєприпасів у Білорусі</div>
                         </div>
                         <div className="fiveSectionsContainer__block__grid__twoCards">
                             <div className="fiveSectionsContainer__block__grid__twoCards__cardMini">
@@ -31,18 +35,20 @@ const Lifestyle = () => {
                     <div className="fiveSectionsContainer__block__grid__columnTwo">
                         <div className="fiveSectionsContainer__block__grid__columnTwo-heading">Новини розділу</div>
                         <div className="fiveSectionsContainer__block__grid__columnTwo-news">
-                            <div className="fiveSectionsContainer__block__grid__columnTwo-urgent">Термінова новина</div>
                             <div className="fiveSectionsContainer__block__grid__columnTwo-desc"><span className="fiveSectionsContainer__block__grid__columnTwo-time">14:59</span>
-                            До Європи за захистом. Що означає позов Ахметова до Європейського суду з прав людини</div>
-                            <div className="fiveSectionsContainer__block__grid__columnTwo-desc"><span className="fiveSectionsContainer__block__grid__columnTwo-time">14:59</span>
-                            Стало відомо, коли ЄС може узгодити сьомий пакет санкцій проти рф</div>
+                            Що подивитися ввечері: топ-5 українських фільмів</div>
+                            <div className="fiveSectionsContainer__block__grid__columnTwo-bold">
+                                <div className="fiveSectionsContainer__block__grid__columnTwo-exclusive">Ексклюзив</div>
+                                <div className="fiveSectionsContainer__block__grid__columnTwo-desc"><span className="fiveSectionsContainer__block__grid__columnTwo-time">14:59</span>
+                                Яке свято відзначають 9 серпня: прикмети, традиції та заборони цього дня</div>
+                            </div>
                             <div className="fiveSectionsContainer__block__grid__columnTwo-video">Новина з відео</div>
                             <div className="fiveSectionsContainer__block__grid__columnTwo-desc"><span className="fiveSectionsContainer__block__grid__columnTwo-time">14:59</span>
-                            До Європи за захистом. Що означає позов Ахметова до Європейського суду з прав людини</div>
+                            Вакарчук виклав неймовірне відео виконання гімну України в Іспанії</div>
                             <div className="fiveSectionsContainer__block__grid__columnTwo-desc"><span className="fiveSectionsContainer__block__grid__columnTwo-time">14:59</span>
-                            "Відбудова України — завдання усього світу": промова Зеленського на конференції в Лугано</div>
+                            Топ-7 повсякденних речей, які не мінялися протягом століть</div>
                             <div className="fiveSectionsContainer__block__grid__columnTwo-desc"><span className="fiveSectionsContainer__block__grid__columnTwo-time">14:59</span>
-                            "Бийся на світлій стороні": як спортсмени, меценати й волонетри рятують життя українців</div>
+                            Арестович розповів про шалений заробіток, трьох дружин і дітей. Відео</div>
                         </div>
                         <div className="fiveSectionsContainer__block__grid__columnTwo-more">Більше</div>
                     </div>
