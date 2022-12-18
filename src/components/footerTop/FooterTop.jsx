d9ccf5119ed73f6b4a05453a31c1b8f38ca4299c
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-12-18 11:34:20 +0200

    Sections

--- src/components/footerTop/FooterTop.jsx
+++ src/components/footerTop/FooterTop.jsx
@@ -3,12 +3,65 @@ import "./footerTop.sass"
 const FooterTop = () => {
     return (
         <div className="footerTopContainer">
-            <div className="footerTopContainer__list"></div>
-            <div className="footerTopContainer__list"></div>
-            <div className="footerTopContainer__list"></div>
-            <div className="footerTopContainer__list"></div>
-            <div className="footerTopContainer__list"></div>
-            <div className="footerTopContainer__list"></div>
+            <div className="footerTopContainer__column">
+                <ul className="footerTopContainer__column__item">
+                    <li className="footerTopContainer__column__item-listHeader">Всі новини</li>
+                </ul>
+                <ul className="footerTopContainer__column__item">
+                    <li className="footerTopContainer__column__item-listHeader">COVID-19</li>
+                </ul>
+                <ul className="footerTopContainer__column__item">
+                    <li className="footerTopContainer__column__item-listHeader">Війна</li>
+                </ul>
+                <ul className="footerTopContainer__column__item">
+                    <li className="footerTopContainer__column__item-listHeader">Політика</li>
+                    <li className="footerTopContainer__column__item-listItem">Україна</li>
+                    <li className="footerTopContainer__column__item-listItem">Світ</li>
+                </ul>
+            </div>
+            <div className="footerTopContainer__column">
+                <ul className="footerTopContainer__column__item">
+                    <li className="footerTopContainer__column__item-listHeader">Здоров’я</li>
+                    <li className="footerTopContainer__column__item-listItem">Дієти</li>
+                    <li className="footerTopContainer__column__item-listItem">Краса</li>
+                    <li className="footerTopContainer__column__item-listItem">Секс</li>
+                    <li className="footerTopContainer__column__item-listItem">Психологія</li>
+                    <li className="footerTopContainer__column__item-listItem">Медицина</li>
+                </ul>
+                <ul className="footerTopContainer__column__item">
+                    <li className="footerTopContainer__column__item-listHeader">Події</li>
+                    <li className="footerTopContainer__column__item-listItem">ДТП</li>
+                    <li className="footerTopContainer__column__item-listItem">Катастрофи</li>
+                    <li className="footerTopContainer__column__item-listItem">Кримінал</li>
+                </ul>
+            </div>
+            <div className="footerTopContainer__column">
+                <ul className="footerTopContainer__column__item">
+                    <li className="footerTopContainer__column__item-listHeader">Лайфстайл</li>
+                    <li className="footerTopContainer__column__item-listItem">Історії з життя</li>
+                    <li className="footerTopContainer__column__item-listItem">Подорожі</li>
+                    <li className="footerTopContainer__column__item-listItem">Кухня</li>
+                    <li className="footerTopContainer__column__item-listItem">Шоубізнес</li>
+                    <li className="footerTopContainer__column__item-listItem">Мода та стиль</li>
+                    <li className="footerTopContainer__column__item-listItem">Зірковий гороскоп</li>
+                </ul>
+                <ul className="footerTopContainer__column__item">
+                    <li className="footerTopContainer__column__item-listHeader">Суспільство</li>
+                    <li className="footerTopContainer__column__item-listItem">Охорона здоров’я</li>
+                    <li className="footerTopContainer__column__item-listItem">Безпека</li>
+                    <li className="footerTopContainer__column__item-listItem">Екологія</li>
+                    <li className="footerTopContainer__column__item-listItem">Релігія</li>
+                </ul>
+            </div>
+            <div className="footerTopContainer__column">
+
+            </div>
+            <div className="footerTopContainer__column">
+
+            </div>
+            <div className="footerTopContainer__column">
+
+            </div>
         </div>
     )
 }
